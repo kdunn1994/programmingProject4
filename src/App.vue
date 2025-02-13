01d111d559eb9909db4772b8acef8e01176baa47
Kevin Dunn <kdunn1@stu.southtexascollege.edu>
2025-02-12 20:22:35 -0600

    added code to AddExercise and App

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,45 @@ import ExerciseDetails from './components/ExerciseDetails.vue';
 import ExerciseList from './components/ExerciseList.vue';
 import Header from './components/Header.vue';
 import ExerciseSummary from './components/ExerciseSummary.vue';
+import AddExercise from './components/AddExercise.vue'
+
+import { ref, computed } from 'vue'
+
+const selectedExercise = ref(null)
+const exercises = ref([])
+
+const handleExerciseSelected = (exercise) => {
+  selectedExercise.value = exercise
+}
+
+const handleExerciseSubmitted = (exerciseData) => {
+  exercises.value.push({
+    id: generateID(),
+    name: exerciseData.name,
+    duration: exerciseData.duration,
+    calories: exerciseData.calories,
+  })
+}
+
+const generateID = () => {
+  return Math.floor(Math.random() * 10000000)
+}
+
+const totalCalories = computed(() => {
+  return exercises.value.reduce((sum, exercise) => sum + exercise.calories, 0)
+})
 
 </script>
 
 
 
 <template>
-  <header></header>
+  <Header></Header>
   <div class="container">
     <ExerciseList @exercise-selected="handleExerciseSelected"></ExerciseList>
     <ExerciseDetails :selectedExercise="selectedExercise"></ExerciseDetails>
     <ExerciseSummary :totalCalories="totalCalories"></ExerciseSummary>
+    <AddExercise @exerciseSubmitted="handleExerciseSubmitted"></AddExercise>
   </div>
 
 
---
+++ src/components/AddExercise.vue
@@ -0,0 +1,36 @@
+<script>
+import { ref, defineEmits } from 'vue'
+
+const name = ref('')
+const duration = ref('')
+
+const emit = defineEmits(['exerciseSubmitted'])
+
+const onSubmit = () => {
+    const exerciseData = {
+        name: name.value,
+        duration: parseFloat(duration.value),
+        calories: parseFloat(duration.value) * 10
+    }
+
+    emit('exerciseSubmitted', exerciseData)
+
+    name.value = ''
+    duration.value = ''
+}
+</script>
+
+<template>
+    <h3>Add a new Exercise</h3>
+    <form @submit.prevent="onSubmit">
+        <div class="form-control">
+            <label for="name">Exercise Name</label>
+             <input type="text" id="name" v-model="name" placeholder="Enter Exercise Name...">            
+        </div>
+        <div class="form-control">
+            <label for="duration">Duration (minutes)</label>
+            <input type="text" id="duration" v-model="duration" placeholder="Enter Duration...">
+        </div>
+        <button class="btn">Add Exercise</button>
+    </form>
+</template>
